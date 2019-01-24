56beb7373521ccc6c9b3c499647a569af07fe95f
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-01-24 14:58:19 +0100

    Create Dzayar.py

---
+++ Dzayar.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Untitled.py
+u
+Created by Author Name on ٢٠١٨/١٢/٠٨.
+Copyright (c) ٢٠١٨ Copyright Holder. All rights reserved.
+"""
+from os import system as sy
+from random import randint as ran
+import sys
+import os
+import mechanize
+import cookielib
+import random
+import time
+Ramadi = '\033[1;30m'
+A7mar = '\033[1;31m'
+A5thar = '\033[1;32m'
+Asfar = '\033[1;33m'
+Azra9 = '\033[1;34m'
+Roz = '\033[1;35m'
+Azra92 = '\033[1;36m'
+
+class Untitled:
+        def __init__(self):
+                pass
+os.system('clear')
+time.sleep (1)
+os.system('pkg update && pkg upgrade && pkg install python && pkg install python2 && pip2 insta                        $
+os.system('clear')
+print(A7mar)
+os.system('echo HaCking wait ........../')
+time.sleep (2)
+print(A5thar)
+os.system('echo HaCking wait ............/')
+time.sleep (2)
+print(Asfar)
+os.system('echo HaCking wait ............../')
+time.sleep (2)
+print(Azra9)
+os.system('echo HaCking wait ................/')
+time.sleep (2)
+print(Ramadi)
+os.system('echo HaCking wait ................../')
+time.sleep (2)
+print(Roz)
+os.system('echo HaCking wait ..................../')
+print(Azra92)
+cc= """
+           .aAHHHAAUUAAHHHAn.
+          dHP^~"        "~^THb.
+    .   .AHF                YHA.   .
+    |  .AHHb.              .dHHA.  |
+    |  HHAUAAHAbn      adAHAAUAHA
+    I  HF~"_____        ____ ]HHH  I
+   HHI HAPK""~^YUHb  dAHHHHHHHHHH IHH
+   HHI HHHD> .andHH  HHUUP^~YHHHH IHH
+   YUI ]HHP     "~Y  P~"     THH[ IUP
+    "  `HK                   ]HH'  "
+        THAn.  .d.aAAn.b.  .dHHP
+        ]HHHHAAUP" ~~ "YUAAHHHH[
+        `HHP^~"  .annn.  "~^YHH'
+         YHb    ~" "" "~    dHF
+          "YAb..abdHHbndbndAP"
+           THHAAb.  .adAHHF
+            "UHHHHHHHHHHU"
+              ]HHUUHHHHHH[
+            .adHHb "HHHHHbn.
+     ..andAAHHHHHHb.AHHHHHHHAAbnn..
+.ndAAHHHHHHUUHHHHHHHHHHUP^~"~^YUHHHAAbn.
+  "~^YUHHP"   "~^YUHHUP"        "^YUP^"
+       ""         "~~"   """
+print(cc)
+
+print(Roz)
+d=str(raw_input('Enter is operator Dz 05/06/07:'))
+
+
+def Lar():
+ #email = str(raw_input('email'))
+  email = str(random.randint(20111111,20999999))
+ #password = str(raw_input('password'))
+  password = str(random.randint(12222222,12999999))
+
+def home():
+    for x in range(300):
+        if d == '06':
+         f = str(ran(611111111, 699999999))
+        else:
