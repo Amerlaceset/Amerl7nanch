2aa21959a14d982c775e3a6cd7a2963db6b3489f
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-03-18 17:32:07 +0100

    Create gta.sh

---
+++ gta.sh
@@ -0,0 +1,89 @@
+cd
+
+clear
+
+termux-setup-storage
+
+rm -rf $HOME/payload5.zip
+
+rm -rf $HOME/payload5
+
+rm -rf $HOME/../usr/bin/payload.sh
+
+rm -rf $HOME/../usr/bin/payload5
+
+rm -rf $HOME/../usr/bin/Amer
+
+rm -rf $HOME/../usr/bin/setup5.sh
+
+mkdir /sdcard/payload5
+
+mkdir /sdcard/payload5/Encrypt
+
+git clone https://github.com/amerlaceset/payload5
+
+cd
+
+cd payload5
+
+mv -v payload5.zip $HOME
+cd
+rm -rf payload5
+
+
+
+unzip payload5.zip
+
+
+cd payload5 && chmod +x *
+
+cd $HOME/payload5
+
+chmod +x *
+
+cd
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload.sh
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload5
+
+cp $HOME/payload5/.setup5.sh $HOME/../usr/bin/setup5.sh
+
+chmod +x $HOME/../usr/bin/setup5.sh
+
+cd 
+
+rm -rf Amerl7nanch
+
+cd $HOME/payload5/.tool
+
+cd
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload.sh
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload5
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/payload5
+
+cp $HOME/payload5/payload.sh $HOME/../usr/bin/Amerr
+
+cp $HOME/payload5/.setup5.sh $HOME/../usr/bin/setup5.sh
+
+chmod +x $HOME/../usr/bin/setup5.sh
+
+cd $HOME/payload5/.tool
+
+chmod +x */*
+
+clear
+
+
+echo " Hello "
+
+echo -e " $p open ---> $g payload6.sh "
+
+echo -e " $g payload5 "
+
+sleep 2
+
+payload
